86201ae5820fda8f4b8e0b886b3235c0963aa415
zhangqingnian <960294873@qq.com>
2019-12-06 16:00:49 +0800

    GoodsList组件  组件拆分

---
+++ src/components/NavBreader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">主页</a>
+        <span>商品列表</span>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+  import '../../static/css/nav-bread.css'
+    export default {
+        components: {},
+        data() {
+            return {}
+        },
+        mounted() {
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import '../static/css/base.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

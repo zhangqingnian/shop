dd4b80cb10842156f62efa647ad74c18bb8f868b
zhangqingnian <960294873@qq.com>
2019-12-06 11:58:06 +0800

    GoodsList组件

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/GoodsList',
+      name: 'GoodsList',
+      component: () => import('@/views/GoodsList')
     }
   ]
 })
